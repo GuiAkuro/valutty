53a9b8d7e654c5f6c5cf51fb87be7ed8957fb8a9
Guilherme Oliveira <g.alves.oliveira@outlook.com>
2024-10-28 23:59:04 -0300

    fastify config

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,1 +1,16 @@
-console.log("server");
+import { env } from "@/env";
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.get("health-check", (_, reply) => {
+  reply.send({
+    server: "is ok",
+  });
+});
+
+app.listen({
+  port: env.PORT,
+});
